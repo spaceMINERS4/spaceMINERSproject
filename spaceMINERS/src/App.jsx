e4f944be46f727920100bfc72fc5e355035e5ca9
ayairis <ayaalae12.2002@gmail.com>
2024-10-05 12:31:36 +0100

    headerrr

--- spaceMINERS/src/App.jsx
+++ spaceMINERS/src/App.jsx
@@ -3,18 +3,14 @@ import './App.css'
 import Header from './components/header/header'
 
 import React from 'react';
-import SolarSystem from './components/3dModel/SolarSystem';
+//import SolarSystem from './components/3dModel/SolarSystem';
 
 
 function App() {
     return (
-        <div style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
-            <SolarSystem />
-            
       <div>
         <Header/>
       </div>
-        </div>
     );
 }
 
