54dd1664eaee6ea3b0a8bbeb485ac35a29cf22a2
FazLaCool <fa.ouardirhi@gmail.com>
2024-10-05 21:42:42 +0100

    jujj

--- spaceMINERS/src/App.jsx
+++ spaceMINERS/src/App.jsx
@@ -1,20 +1,14 @@
-
-import './App.css'
-import Header from './components/header/header'
-import SolarSystem from './components/3Dmodel/model';
+import './App.css';
+import Header from './components/header/header';
+import NEOVisualization from './components/3Dmodel/asteroid.jsx';
 import React from 'react';
-
 
 function App() {
     return (
-      <div>{/* 
-      <div style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
-      <SolarSystem />
-      </div> */}
-      <div>
-        <Header/>
-        <SolarSystem/>
-      </div></div>
+        <div style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
+            <Header />
+            <NEOVisualization />
+        </div>
     );
 }
 
